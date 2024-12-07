4701d0742c82ab058ed1f51e19f25a97be48d72b
udaykiriti <123718855+udaykiriti@users.noreply.github.com>
2024-12-08 03:59:23 +0530

    Update FacultyViewUsers.js

--- src/facultycomponents/FacultyViewUsers.js
+++ src/facultycomponents/FacultyViewUsers.js
@@ -25,7 +25,7 @@ const FacultyViewUsers = () => {
     const fetchUsers = async () => {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/users");
+        const response = await fetch("https://onlineworkshop-server-production.up.railway.app/api/users");
         if (!response.ok) throw new Error("Network response was not ok");
         const data = await response.json();
         setUsers(data);
