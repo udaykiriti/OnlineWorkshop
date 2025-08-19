9ca33ebd3498a906912056c7bea759f9689c7635
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-08-19 07:54:29 +0530

    Update ProtectedRoute.js

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -4,16 +4,12 @@ import { Navigate } from "react-router-dom";
 const ProtectedRoute = ({ element, requiredRole }) => {
   const isAuthenticated = localStorage.getItem("username") !== null;
   const userRole = localStorage.getItem("role"); 
-
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
   }
-
   if (requiredRole && userRole !== requiredRole) {
     return <Navigate to="/login" />;
   }
-
   return element;
 };
-
 export default ProtectedRoute;
